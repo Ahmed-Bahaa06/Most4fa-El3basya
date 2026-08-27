00f09e41e2ec391bfd382e4fa7ba981585ec585b
janaeldereny <janaeldereny@gmail.com>
2026-08-27 22:38:40 +0300

    Interactind and dash

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float dashForce = 10f;
+    [SerializeField] private float dashCooldown = 1f;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private float lastDashTime;
 
     private void Awake()
     {
@@ -15,19 +18,59 @@ public class PlayerMovement : MonoBehaviour
     private void OnEnable()
     {
         if (GameInputManager.Instance != null)
+        {
             GameInputManager.Instance.OnMove += HandleMove;
+            GameInputManager.Instance.OnDash += HandleDash;
+            GameInputManager.Instance.OnInteract += HandleInteract;
+            
+        }
     }
 
     private void OnDisable()
     {
         if (GameInputManager.Instance != null)
+        {
             GameInputManager.Instance.OnMove -= HandleMove;
+            GameInputManager.Instance.OnDash -= HandleDash;
+            GameInputManager.Instance.OnInteract -= HandleInteract;
+            
+        }
     }
 
     private void HandleMove(Vector2 input)
     {
         moveInput = input;
     }
+
+    private void HandleDash()
+    {
+        if (Time.time >= lastDashTime + dashCooldown)
+        {
+            if (moveInput != Vector2.zero)
+            {
+                Vector3 dashDir = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+                rb.AddForce(dashDir * dashForce, ForceMode2D.Impulse);
+                lastDashTime = Time.time;
+            }
+        }
+    }
+
+    private void HandleInteract()
+    {
+        float interactRange = 2f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+
+        foreach (Collider collider in colliderArray)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactable.Interact();
+                break;
+            }
+        }
+    }
+
+
 
 
     private void FixedUpdate()
@@ -40,3 +83,6 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = moveInput * moveSpeed;
     }
 }
+
+
+
