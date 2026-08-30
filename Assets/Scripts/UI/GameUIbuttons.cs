e0344a59f8027351c5862f4aee6cd0535b4059ee
Joy Magdy <joymagdy257@gmail.com>
2026-08-30 20:12:52 +0300

    commit

--- Assets/Scripts/UI/GameUIbuttons.cs
+++ Assets/Scripts/UI/GameUIbuttons.cs
@@ -15,5 +15,21 @@ public class GameUIbuttons : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void ResumeGame()
+    {
+        KhosaryCode.Core.GameManager gm = FindAnyObjectByType<KhosaryCode.Core.GameManager>();
+        if (gm != null)
+        {
+            gm.TogglePause();
+        }
+        else
+        {
+            // Failsafe if GameManager is missing
+            Time.timeScale = 1f;
+            KhosaryCode.UI.HUDManager hud = FindAnyObjectByType<KhosaryCode.UI.HUDManager>();
+            if (hud != null) hud.HideAllPanels();
+        }
+    }
 }
 
